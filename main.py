943f6bfc9e7e73291ab866052d8f7afa0e64fe63
StuoodJ <Jessebetea@gmail.com>
2024-01-29 22:00:26 -0500

    clean up code

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ sH = 400
 screen = pygame.display.set_mode((sW, sH))
 time = pygame.time
 clock = time.Clock()
-running = True
+#/variables
 cash = 0
 dt = 0
 bX = 50
 bY = 50
 bW = 80
 bH = 40
+#/run
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -28,16 +30,19 @@ while running:
 
     drawtext(screen, "Cash:", text_font, (255,255,255), 20, 20)
     drawtext(screen, str(cash), text_font, (255,255,255), 80, 20)
+    
     #/buttons
+    
     button = pygame.draw.rect(screen, (255,0,0), (bX, bY, bW, bH))
     drawtext(screen, "1+", text_font, (255,255,255), bX+30, bY+12.5)
+    
     lM, MM, RM = pygame.mouse.get_pressed()
+    
     if mousex >= bX and mousex <= bX+bW and mousey >= bY and mousey <= bY+bH:
         if lM is True:
             cash += 1
             time.delay(150)
 
-            
     #/
     pygame.display.flip()
 
